e04985830255077bca6dedd9e2483b29c4d334c0
DKrey06 <mrdmitry2006@mail.ru>
2025-04-23 10:41:17 +1000

    Trash1

--- COFFEFU.py
+++ COFFEFU.py
@@ -8,6 +8,9 @@ PLAYER_SPEED = 10
 
 BACKGROUND_COLOR = (207, 229, 250)
 PLAYER_COLOR = (120, 94, 130)
+GRAVITY = 0.8
+JUMP_STRENGTH = -15
+MAX_FALL_SPEED = 15  
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -37,61 +40,66 @@ class Player(pygame.sprite.Sprite):
     global TEST_LEVEL
     def __init__(self):
         super().__init__()
-
-        self.image = pygame.Surface((200, 200))
-        self.image.fill(PLAYER_COLOR)
-
+        self.image = pygame.Surface((100, 100))
+        self.image.fill(PLAYER_COLOR) 
         self.rect = self.image.get_rect()
-
         self.rect.centerx = SCREEN_WIDTH // 2
-        self.rect.bottom = SCREEN_HEIGHT - 20
-
+        self.rect.bottom = SCREEN_HEIGHT 
         self.change_x = 0
         self.change_y = 0
-
         self.facing_right = True
+        self.on_ground = True 
 
     def update(self):
 
         self.rect.x += self.change_x
-
+        
         if self.rect.left < 0:
             self.rect.left = 0
         if self.rect.right > SCREEN_WIDTH+TEST_LEVEL:
             self.rect.right = SCREEN_WIDTH+TEST_LEVEL
-
+            
+        if not self.on_ground:
+            self.change_y += GRAVITY
+            if self.change_y > MAX_FALL_SPEED:
+                self.change_y = MAX_FALL_SPEED
+        
         self.rect.y += self.change_y
+        
+
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+            self.change_y = 0
+            self.on_ground = True
+        else:
+            self.on_ground = False
+            
 
         if self.rect.top < 0:
             self.rect.top = 0
-        if self.rect.bottom > SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
-
+            self.change_y = 0  
 
     def go_left(self):
         self.change_x = -PLAYER_SPEED
         if self.facing_right:
             self.flip()
-            self.facing_right = False
+            self.facing_right = False 
 
     def go_right(self):
         self.change_x = PLAYER_SPEED
-        if not self.facing_right:
+        if not self.facing_right: 
             self.flip()
-            self.facing_right = True
+            self.facing_right = True 
 
-    def go_up(self):
-        self.change_y = -PLAYER_SPEED
-
-    def go_down(self):
-        self.change_y = PLAYER_SPEED
+    def jump(self):
+        if self.on_ground:
+            self.change_y = JUMP_STRENGTH
+            self.on_ground = False
 
     def stop(self):
         self.change_x = 0
-        self.change_y = 0
 
     def flip(self):
-
         self.image = pygame.transform.flip(self.image, True, False)
 
 ###Стартовое окно (Димка)###
@@ -241,21 +249,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN: 
                 if event.key == pygame.K_a:
                     player.go_left()
                 elif event.key == pygame.K_d:
                     player.go_right()
-                elif event.key == pygame.K_w:
-                    player.go_up()
-                elif event.key == pygame.K_s:
-                    player.go_down()
-                elif event.key == pygame.K_ESCAPE:
+                if event.key == pygame.K_SPACE or event.key == pygame.K_w:
+                    player.jump()
+                elif event.key == pygame.K_ESCAPE: 
                     running = False
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_a or event.key == pygame.K_d:
+            elif event.type == pygame.KEYUP: 
+                if event.key == pygame.K_a and player.change_x < 0:
                     player.stop()
-                elif event.key == pygame.K_w or event.key == pygame.K_s:
+                elif event.key == pygame.K_d and player.change_x > 0:
                     player.stop()
 
         all_sprites.update()
